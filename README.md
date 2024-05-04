18fe3181452f60d5f5bc8eaca5d16c674c2f02e5
kyratzakos <kyratzakos.ap@gmail.com>
2024-05-04 18:57:49 +0300

    Completed version of the game
    - new readme.md
    - add requirements.txt for the dependecies

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# A wordle app
-A wordle app made by me using Python
+# Wordle Game
+
+This is a simple implementation of the Wordle game in Python. In this game, the player needs to guess a 5-letter word, and the program will provide feedback on the correctness of each letter in the guessed word.
+
+## Requirements
+
+To run this game, you need to have the following dependencies installed:
+
+-   `RandomWords`: A Python library from [tomislater](https://github.com/tomislater) to generate random English words ([GitHub Repo](https://github.com/tomislater/RandomWords)).
+-   `rich`: A Python library for rich text and beautiful formatting in the terminal.
+
+You can install these dependencies by running:
+
+```
+pip install -r requirements.txt
+```
+
+## How to Play
+
+1. Clone this repository to your local machine.
+2. Install the dependencies as mentioned above.
+3. Run the `wordle.py` script using Python:
+
+```
+python wordle.py
+```
+
+4. The game will prompt you to enter a 5-letter word.
+5. Keep guessing words until you correctly guess the word or exhaust your allowed guesses.
+6. The program will provide feedback on each guess, indicating correct letters in the correct position, correct letters in the wrong position, and incorrect letters.
+7. The game ends when you correctly guess the word or use all allowed guesses.
+
+Have fun playing Wordle!
--- worldeapp.py
+++ worldeapp.py
@@ -1,4 +1,6 @@
 from random_words import RandomWords
+from rich.prompt import Prompt
+from rich.console import Console
 from rich import *
 
 DOTS = {
@@ -7,6 +9,9 @@ DOTS = {
     'incorrect_letter': f'[black on white]o[/]'
 }
 
+GUESS_STATEMENT = 'Enter a 5-letter word: '
+ALLOWED_GUESSES = 6
+
 
 def getaword():
     # generates a valid 5-letter word
@@ -14,7 +19,7 @@ def getaword():
     validword = rw.random_words(min_letter_count=5)
     while len(validword[0]) != 5:
         validword = rw.random_words(min_letter_count=4)
-    return validword
+    return validword[0]
 
 
 def correct_place(letter):
@@ -37,32 +42,58 @@ def game_is_over(guess, word, guesses, rounds):
 
 
 def guess_check(guess_word, correct_word):
-    # checks if the guess is correct letter per letter
     guessed = []
     wordle_pattern = []
+    correct_letters = set(correct_word)
+    
     for i, letter in enumerate(guess_word):
-        if correct_word[i] == guess_word[i]:
+        if correct_word[i] == letter:
             guessed += correct_place(letter)
             wordle_pattern.append(DOTS['correct_place'])
-        elif letter in correct_word:
+        elif letter in correct_letters:
             guessed += correct_letter(letter)
             wordle_pattern.append(DOTS['correct_letter'])
+            correct_letters.remove(letter)
         else:
             guessed += incorrect_letter(letter)
             wordle_pattern.append(DOTS['incorrect_letter'])
     return ''.join(guessed), ''.join(wordle_pattern)
 
 
+
 def play():
     # main funtion of the game
-    tries = 0
+    end_of_game = False
     word = getaword()
-    while not game_is_over() :
-        tries += 1
-        g
-
+    already_guessed = []
+    all_words_guessed = []
+    full_wordle_pattern = []
     print(word)
+    
+    while not end_of_game :
+        guess = Prompt.ask(GUESS_STATEMENT).lower()
+        while len(guess) != 5 or guess in already_guessed:
+            if guess in already_guessed:
+                console.print("[red]You've already guessed this word\n[/]")
+            else:
+                console.print('[red]Please enter a 5-letter word\n[/]')
+            guess = Prompt.ask(GUESS_STATEMENT).upper()
+        already_guessed.append(guess)
+        guessed, pattern = guess_check(guess, word)
+        all_words_guessed.append(guessed)
+        full_wordle_pattern.append(pattern)
+
+        console.print(*all_words_guessed, sep="\n")
+        if guess == word or len(already_guessed) == ALLOWED_GUESSES:
+            end_of_game = True
+    if len(already_guessed) == ALLOWED_GUESSES and guess != word:
+        console.print(f"\n[red]WORDLE X/{ALLOWED_GUESSES}[/]")
+        console.print(f'\n[green]Correct Word: {word}[/]')
+    else:
+        console.print(f"\n[green]WORDLE {len(already_guessed)}/{ALLOWED_GUESSES}[/]\n")
+    console.print(*full_wordle_pattern, sep="\n")
 
 
 if __name__ == '__main__':
+    console = Console()
     play()
